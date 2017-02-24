d911c3bfea8296a13cc1f91019770251350b7989
Shashi Prabhakar <shaship@yahoo.com>
2017-02-24 15:12:20 -0800

    Update config.rb
    fixes from migration to 1.0 mina

--- lib/mina/config.rb
+++ lib/mina/config.rb
@@ -11,15 +11,15 @@ config_file = 'config/deploy.yml'
 set :config, YAML.load(File.open(config_file)).with_indifferent_access if File.exists? config_file
 set :rails_env, ENV['to'] || :staging
 
-unless config.nil?
+unless fetch(:config).nil?
   envs = []
-  config.each {|k,v| envs << k}
+  fetch(:config).each {|k,v| envs << k}
 
   set :environments, envs
 end
 
 unless environments.nil?
-  environments.each do |environment|
+  fetch(:environments).each do |environment|
     desc "Set the environment to #{environment}."
     task(environment) do
       set :rails_env, environment
@@ -36,11 +36,11 @@ unless environments.nil?
 
       set :ruby_version, File.read('.ruby-version')
 
-      invoke :"rvm:use[#{ruby_version}]"
+      invoke :"rvm:use[#{fetch(:ruby_version)}]"
     end
   end
 
-  unless environments.include?(ARGV.first)
+  unless fetch(:environments).include?(ARGV.first)
     invoke default_env
   end
 end
